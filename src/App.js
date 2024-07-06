ce82cbb4be0e4fbea738be7aa069171947499ff3
Heeral Sahu <92670621+HeeralSahu@users.noreply.github.com>
2024-07-07 00:50:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import LoginSignUp from './pages/LoginSignUp';
+import Shop from './pages/Shop'
+import ShopCategory from './pages/ShopCategory';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter> 
+        <Navbar />
+        <Routes>
+          <Route path='/' element = {<Shop/>}/>
+          <Route path='/men' element={<ShopCategory category='men'/>}/>
+          <Route path='/women' element={<ShopCategory category='women'/>} />
+          <Route path='/kids' element={<ShopCategory category='kids'/>} />
+          <Route path='/product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart />}/>
+          <Route path='/login' element={<LoginSignUp/>}/>
+      </Routes>
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
